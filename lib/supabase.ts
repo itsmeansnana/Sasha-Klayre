93a7930e7453a722089789d5dd35b4cc16e850d2
itsmeansnana <0x000l0llll@gmail.com>
2025-09-29 16:54:17 +0700

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,14 @@
-import { createClient } from '@supabase/supabase-js'
-
+// lib/supabase.ts
+import { createClient } from "@supabase/supabase-js";
 
 export const supabaseAnon = () =>
-createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
-
+  createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
 
 export const supabaseService = () =>
-createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE!)
+  createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE!
+  );
